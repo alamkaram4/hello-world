3b3cc9b765135e485b7ebbab87327e4c0740e255
Raghunadha4 <raghunadha@duck.com>
2023-12-24 21:32:51 +0000

    first commit

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,20 @@
+// src/main/java/com/example/demo/GreetingController.java
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @Value("${greeting.message}")
+    private String message;
+
+    @GetMapping("/greeting")
+    public String greeting() {
+    	
+        return message+" java";
+    }
+}
+
